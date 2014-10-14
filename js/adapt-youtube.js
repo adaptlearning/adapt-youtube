546d7fb3bc7e9cde83a54591a16781fb6f0c43c6
Oliver Foster <oliver.foster@kineo.com>
2014-10-14 11:43:47 +0100

    Saving changes

--- js/adapt-youtube.js
+++ js/adapt-youtube.js
@@ -16,7 +16,43 @@ define(function(require) {
         },
 
         postRender: function() {
+        	this.setReadyStatus();
+            this.setupEventListeners();
+        },
 
+        inview: function(event, visible, visiblePartX, visiblePartY) {
+            if (visible) {
+                if (visiblePartY === 'top') {
+                    this._isVisibleTop = true;
+                } else if (visiblePartY === 'bottom') {
+                    this._isVisibleBottom = true;
+                } else {
+                    this._isVisibleTop = true;
+                    this._isVisibleBottom = true;
+                }
+
+                if (this._isVisibleTop && this._isVisibleBottom) {
+                    this.$('.component-inner').off('inview');
+                    this.setCompletionStatus();
+                }
+                
+            }
+        },
+
+        setupEventListeners: function() {
+            this.completionEvent = (!this.model.get('_setCompletionOn')) ? 'play' : this.model.get('_setCompletionOn');
+            if (this.completionEvent !== "inview") {
+                //this.mediaElement.addEventListener(this.completionEvent, _.bind(this.onCompletion, this));
+                
+            } else {
+                this.$('.component-widget').on('inview', _.bind(this.inview, this));
+            }
+        },
+
+        onCompletion: function() {
+            this.setCompletionStatus();
+            // removeEventListener needs to pass in the method to remove the event in firefox and IE10
+            this.mediaElement.removeEventListener(this.completionEvent, this.onCompletion);
         }
         
     });
