61d278e4155686dda9ff6b482574e675c1d26dae
Chris Steele <chris.steele@kineo.com>
2014-10-21 16:23:11 +0100

    Uses YouTube iframe API to listen for play end

--- js/adapt-youtube.js
+++ js/adapt-youtube.js
@@ -10,7 +10,24 @@ define(function(require) {
 	var Adapt = require('coreJS/adapt');
 
     var youtube = ComponentView.extend({
-        
+        defaults:function() {
+            return {
+                player:null
+            }
+        },
+
+        initialize: function() {
+            ComponentView.prototype.initialize.apply(this);
+
+            if (window.onYouTubeIframeAPIReady === undefined) {
+                window.onYouTubeIframeAPIReady = function() {
+                    console.info('YouTube iframe API loaded');
+                    Adapt.youTubeIframeAPIReady = true;
+                    Adapt.trigger('youTubeIframeAPIReady');
+                };
+                $.getScript('https://www.youtube.com/iframe_api');
+            }
+        },
 
         preRender: function() {
             this.listenTo(Adapt, 'device:resize', this.onScreenSizeChanged);
@@ -27,18 +44,16 @@ define(function(require) {
 
         postRender: function() {
             //FOR HTML/HBS Paramenters: https://developers.google.com/youtube/player_parameters
-        	this.setReadyStatus();
-            this.setupEventListeners();
+            if (Adapt.youTubeIframeAPIReady === true) this.onYouTubeIframeAPIReady();
+            else Adapt.once('youTubeIframeAPIReady', this.onYouTubeIframeAPIReady, this)
         },
-
     
         setupEventListeners: function() {
             //Completion events play/inview
             this.completionEvent = (!this.model.get('_setCompletionOn')) ? 'play' : this.model.get('_setCompletionOn');
             if (this.completionEvent !== "inview") {
-                //TODO: youtube on play completion using https://developers.google.com/youtube/iframe_api_reference
-                //calback to this.onCompletion
-
+                //https://developers.google.com/youtube/iframe_api_reference
+                this.player.addEventListener('onStateChange', _.bind(this.onPlayerStateChange, this));
             } else {
                 this.$('.component-widget').on('inview', _.bind(this.onInview, this));
             }
@@ -61,6 +76,17 @@ define(function(require) {
                 }
                 
             }
+        },
+
+        onYouTubeIframeAPIReady: function() {
+            console.info('onYouTubeIframeAPIReady');
+            this.player = new YT.Player(this.$('iframe').get(0));
+            this.setReadyStatus();
+            this.setupEventListeners();
+        },
+
+        onPlayerStateChange: function(event) {
+            if (event.data == YT.PlayerState.ENDED) this.onCompletion();
         },
 
         onCompletion: function() {
