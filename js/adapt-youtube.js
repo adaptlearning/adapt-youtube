49d9d9317b3374332eaa578e9fed9ee35a752757
Oliver Foster <oliver.foster@kineo.com>
2022-03-24 14:36:12 +0000

    issue/2799 Removed redundant behaviour

--- js/adapt-youtube.js
+++ js/adapt-youtube.js
@@ -1,8 +1,8 @@
-import Adapt from 'core/js/adapt';
+import components from 'core/js/components';
 import ComponentModel from 'core/js/models/componentModel';
 import YouTubeView from './YouTubeView';
 
-export default Adapt.register('youtube', {
+export default components.register('youtube', {
   model: ComponentModel.extend({}),
   view: YouTubeView
 });
